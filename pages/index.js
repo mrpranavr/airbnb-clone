669dbdb2358db0fd9d5976ccd4bab8e012a31972
Pranav R <pranav81234@gmail.com>
2023-01-24 12:10:41 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "@/components/Header";
+import Header from "@/components/Header.js";
 import Banner from "@/components/Banner";
 
 import { Nunito } from "@next/font/google";
