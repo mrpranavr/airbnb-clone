df7f3aef8adcfe5087dcb999bb523f0e980cefeb
Pranav R <pranav81234@gmail.com>
2023-01-24 12:21:18 +0530

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from "@/components/Header.js";
+import Header from "@/components/header";
 import Banner from "@/components/Banner";
 
 import { Nunito } from "@next/font/google";
